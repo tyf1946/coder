bf90e69a0fe78dfce9b9d44fbdd1c68ac453bc71
zhulonglong <zhulonglong@yituikecorp.com>
2020-03-15 11:18:11 +0800

    "message :  "

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 git add -A
-tim = $(date)
-message = "$(tim) : $(0)"
+tim=$(date)
+message="$(tim) : $(0)"
 git commit -m "\"message : $(message) \" "
 git push origin master
+
 echo message
